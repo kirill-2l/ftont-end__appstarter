ef550aa37975edc2a612747e2c7fa14605d09e42
Kirill Petrov <kirill@Kirills-MacBook-Air.local>
2019-05-04 16:50:24 +0300

    update

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/* src/app.js */
+
+import "magnific-popup";
+
+// Styles
+import 'styles/_app.scss';
+
+$(document).ready(() => {
+    console.log('Ready!');
+
+    require('scripts/main');
+});
---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+console.log('Demo!');
+
+$(document).ready(function(){
+  $('.hamburger').on('click', function(){
+    $('.navigation').toggleClass('opened');
+    $('.hamburger').toggleClass('hamburger__active');
+    return false;
+  });
+});
+
+
+$( document ).ready(function() {
+	$('.video__popup-link').magnificPopup({
+	 type:'inline',
+	 midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+ });
+
+ });
